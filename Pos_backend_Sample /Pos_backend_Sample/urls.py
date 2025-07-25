50c24a3655895d576bb879dd405769b96679e5ac
HARIKARAN G <harikarang007@gmail.com>
2025-07-25 20:32:12 +0530

    Delete Pos_backend_Sample  directory

--- Pos_backend_Sample /Pos_backend_Sample/urls.py
+++
@@ -1,9 +0,0 @@
-# project_root/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('Pos_App.urls')),  # ✅ This line connects your Pos_App
-]
